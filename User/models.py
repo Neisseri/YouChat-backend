2a0055d486db5df3e5e0363ad931a36be845e524
xzythu <1159108732@qq.com>
2023-03-19 17:24:49 +0800

    [#1]add user and friend model

--- User/models.py
+++ User/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from utils import utils_time
+
+from constants.user import MAX_CHAR_LENGTH
 
 # Create your models here.
+    
+class Friend(models.Model):
+    friendList = []
+    newFriendList = []
+
+class User(models.Model):
+    user_id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=MAX_CHAR_LENGTH, unique=True)
+    nickname = models.CharField(max_length=MAX_CHAR_LENGTH, unique=False)
+    password = models.CharField(max_length=MAX_CHAR_LENGTH, unique= False)
+    register_time = models.FloatField(default=utils_time.get_timestamp)
+    login_time = models.FloatField(default=utils_time.get_timestamp)
+    email = models.CharField(max_length=MAX_CHAR_LENGTH, unique=True)
+    friend = Friend()
+
+    class Meta:
+        indexes = [models.Index(fields=["name"])]
+
+    def __str__(self) -> str:
+        return self.name
