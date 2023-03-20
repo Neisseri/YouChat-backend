a866719b8065b58fe54d44cac3460777705a7cad
jinyu <jinyuchen_derick@outlook.com>
2023-03-20 12:36:34 +0800

    [#1]add group model

--- User/models.py
+++ User/models.py
@@ -4,10 +4,6 @@ from utils import utils_time
 from constants.user import MAX_CHAR_LENGTH
 
 # Create your models here.
-    
-class Friend(models.Model):
-    friendList = []
-    newFriendList = []
 
 class User(models.Model):
     user_id = models.BigAutoField(primary_key=True)
@@ -17,8 +13,8 @@ class User(models.Model):
     register_time = models.FloatField(default=utils_time.get_timestamp)
     login_time = models.FloatField(default=utils_time.get_timestamp)
     email = models.CharField(max_length=MAX_CHAR_LENGTH, unique=True)
-    friends = models.ManyToManyField('self',  through='Contact')
-    friend_requests = models.ManyToManyField('self',  through='FriendRequest', symmetrical=False)
+    friends = models.ManyToManyField('self',  through='Contacts')
+    friend_requests = models.ManyToManyField('self',  through='FriendRequests', symmetrical=False)
 
     class Meta:
         indexes = [models.Index(fields=["name"])]
@@ -26,7 +22,18 @@ class User(models.Model):
     def __str__(self) -> str:
         return self.name
     
-class Contact(models.Model):
+class Group(models.Model):
+    group_id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=MAX_CHAR_LENGTH, unique=True)
+    members = models.ManyToManyField(User, through='Membership')
+
+    class Meta:
+        indexes = [models.Index(fields=["name"])]
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Contacts(models.Model):
     user = models.ForeignKey(User, related_name='friend_backward', on_delete=models.CASCADE)
     friend = models.ForeignKey(User, related_name='friend', on_delete=models.CASCADE)
     update_time = models.FloatField(default=utils_time.get_timestamp)
@@ -37,7 +44,7 @@ class Contact(models.Model):
     def __str__(self) -> str:
         return f'{self.user} and {self.friend} are friends'
 
-class FriendRequest(models.Model):
+class FriendRequests(models.Model):
     sendee = models.ForeignKey(User, related_name='receiver', on_delete=models.CASCADE)
     sender = models.ForeignKey(User, related_name='sender', on_delete=models.CASCADE)
     update_time = models.FloatField(default=utils_time.get_timestamp)
@@ -47,3 +54,11 @@ class FriendRequest(models.Model):
 
     def __str__(self) -> str:
         return f'{self.sender} sent a request to {self.sendee}'
+    
+class Membership(models.Model):
+    group = models.ForeignKey(Group, related_name = 'groups', on_delete=models.CASCADE)
+    member = models.ForeignKey(User, related_name = 'members', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'{self.member} belongs to {self.group}'
+
