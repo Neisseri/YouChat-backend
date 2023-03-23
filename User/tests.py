f7d765810647ddd1c43001fc94cd364228248386
xzythu <1159108732@qq.com>
2023-03-23 21:43:21 +0800

    [#6]deploy django on gitlab add test

--- User/tests.py
+++ User/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from User.models import User
 
 # Create your tests here.
+class UserTests(TestCase):
+
+    def setUp(self):
+        User.objects.create(name = "swim", password = "abc1234567", nickname = "Alice", email = "17@swim.com")
+
+    def post_user(self, name, password, nickname, email):
+        payload = {
+            "userName": name,
+            "password": password,
+            "nickname": nickname,
+            "email": email
+        }
+        return self.client.post("/people/user", data=payload, content_type="application/json")
