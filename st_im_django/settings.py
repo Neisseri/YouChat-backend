bd1d969dcb768d285da690ebaa667961e02f3f4e
xzythu <1159108732@qq.com>
2023-03-24 11:13:49 +0800

    [#6]deploy django on gitlab change yml

--- st_im_django/settings.py
+++ st_im_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#7s&l=#y&mdebr_5b9!p^2ce-6vc!a9i291q6#6+5fsn=0&*xu"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.getenv('DEPLOY') == None:
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = []
 
