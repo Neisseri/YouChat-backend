5d14a0c5bc5363420528072ba8b6b645f7679efe
tyt21 <tyt21@mails.tsinghua.edu.cn>
2023-04-08 08:28:52 +0800

    [#25] add setup

--- Session/tests.py
+++ Session/tests.py
@@ -1,5 +1,12 @@
 from django.test import TestCase
 import random
 from Session.models import Session, UserAndSession, Message
+from User.models import User
 
 # Create your tests here.
+class SessionTests(TestCase):
+    
+    # Initializer
+    def setUp(self):
+        alice = User.objects.create(name = "swim17", password = "abc1234567", 
+                                    nickname = "Alice", email = "17@swim.com")
