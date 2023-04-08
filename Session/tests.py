c57df89d3c36eb08225c68e7c22700295ca4d079
tyt21 <tyt21@mails.tsinghua.edu.cn>
2023-04-08 09:01:58 +0800

    [#25] design get image api

--- Session/tests.py
+++ Session/tests.py
@@ -10,3 +10,12 @@ class SessionTests(TestCase):
     def setUp(self):
         alice = User.objects.create(name = "swim17", password = "abc1234567", 
                                     nickname = "Alice", email = "17@swim.com")
+        
+        bob = User.objects.create(name = "swim11", password = "abc12345678", 
+                                    nickname = "Bob", email = "11@swim.com")
+        
+    # Utility Functions
+    def get_image(self, user_id):
+        return self.client.get(f"/session/image/{user_id}", content_type="application/json")
+    
+    
