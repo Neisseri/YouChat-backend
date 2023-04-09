18dcd029332b434ecbf1dc32da4f580022a4759d
tyt21 <tyt21@mails.tsinghua.edu.cn>
2023-04-09 19:31:21 +0800

    fix: deploy csrf

--- st_im_django/settings.py
+++ st_im_django/settings.py
@@ -154,4 +154,4 @@ CHANNEL_LAYERS = {
     },
 }
 
-CSRF_TRUSTED_ORIGINS = ['https://st-im-django-swimtogether.app.secoder.net/','https://127.0.0.1:3000']
+CSRF_TRUSTED_ORIGINS = ['http://st-im-next-swimtogether.app.secoder.net', 'http://localhost:3000']
