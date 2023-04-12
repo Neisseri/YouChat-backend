77de301b2bf0edaaaee69450d254a43e23bdb627
jinyu <jinyuchen_derick@outlook.com>
2023-04-13 07:49:20 +0800

    fix: fix consumers

--- WebSocket/consumers.py
+++ WebSocket/consumers.py
@@ -248,7 +248,7 @@ class MyConsumer(AsyncWebsocketConsumer):
 
     # Receive message from room group
     async def chat_message(self, event):
-        message = event["message"]
+        message = dict(event["message"])
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps(message))
