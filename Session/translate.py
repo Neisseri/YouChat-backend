5991496d943bc2aa4208c65040fe9d1a319ab3b8
tyt21 <tyt21@mails.tsinghua.edu.cn>
2023-04-16 20:07:29 +0800

    design: translate function(#42)

--- Session/translate.py
+++ Session/translate.py
@@ -1,14 +1,11 @@
 import requests
 
 def translate(language, text):
-    data1 = { 'doctype': 'json', 'type': 'auto','i': '你吃饭了吗？' }
-    data2 = { 'doctype': 'json', 'type': 'auto','i': 'Did you eat?' }
-    r = requests.get("http://fanyi.youdao.com/translate",params=data1)
+    # data1 = { 'doctype': 'json', 'type': 'auto','i': '你吃饭了吗？' }
+    data = { 'doctype': 'json', 'type': 'auto','i': text }
+    r = requests.get("http://fanyi.youdao.com/translate", params=data)
     result = r.json()
-    print(result)
-    r = requests.get("http://fanyi.youdao.com/translate",params=data2)
-    result = r.json()
-    print(result)
+    return result
 
 if __name__ == '__main__':
     text = 'Did you eat?'
