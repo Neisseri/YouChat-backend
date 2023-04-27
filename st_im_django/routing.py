be2872937b2a1467d2c5fd35346a999e34537d8f
tyt21 <tyt21@mails.tsinghua.edu.cn>
2023-04-27 11:47:11 +0800

    add: urls(#53)

--- st_im_django/routing.py
+++ st_im_django/routing.py
@@ -9,6 +9,11 @@ from django.core.asgi import get_asgi_application
 
 import os
 
+# for voice communication
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "st_im_django.settings")
 
 application = ProtocolTypeRouter({
@@ -18,4 +23,8 @@ application = ProtocolTypeRouter({
             WebSocket.routing.websocket_urlpatterns
         )
     ),
+    
+    # "websocket": URLRouter([
+    #     path("ws/webrtc/", consumers.WebRTCConsumer.as_asgi()),
+    # ])
 })
