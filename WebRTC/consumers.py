8c8a21f20a34fc9f11209de65c2922e437b33014
tyt21 <tyt21@mails.tsinghua.edu.cn>
2023-04-27 10:54:39 +0800

    design: comsumers(#53)

--- WebRTC/consumers.py
+++ WebRTC/consumers.py
@@ -1,11 +1,28 @@
-from channels.generic.websocket import WebsocketConsumer
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class WebRTCConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+# reference: https://juejin.cn/post/6982339341960331301
+class WebRTCConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.channel_layer.group_add("webrtc", self.channel_name)
+        await self.accept()
 
-    def disconnect(self, close_code):
-        pass
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard("webrtc", self.channel_name)
 
-    def receive(self, text_data):
-        self.send(text_data=text_data)
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        await self.channel_layer.group_send(
+            "webrtc",
+            {
+                "type": "webrtc_message",
+                "message": message,
+            }
+        )
+
+    async def webrtc_message(self, event):
+        message = event['message']
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
