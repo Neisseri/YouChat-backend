d3296f710eecb5c9ce61aeed819ef35d8430110f
jinyu <jinyuchen_derick@outlook.com>
2023-05-23 23:23:27 +0800

    fix: test(#62)

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-coverage run --source st_im_django,User,utils -m pytest --junit-xml=xunit-reports/xunit-result.xml
+coverage run --source st_im_django,User,utils,Session,Websocket -m pytest --junit-xml=xunit-reports/xunit-result.xml
 ret=$?
 coverage xml -o coverage-reports/coverage.xml
 coverage report
